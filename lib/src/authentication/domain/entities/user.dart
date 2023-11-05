3e815e9336c3c22b2849abfcab066c5342f47eed
pramod <kumarpramod559@gmail.com>
2023-11-06 00:59:18 +0530

    sunday_night

--- lib/src/authentication/domain/entities/user.dart
+++ lib/src/authentication/domain/entities/user.dart
@@ -9,13 +9,13 @@ class User extends Equatable {
   });
   const User.empty()
       : this(
-          id: 1,
+          id: '1',
           createdAt: '_empty.createdAt',
           name: '_empty.name',
           avatar: '_empty.avatar',
         );
 
-  final int id;
+  final String id;
   final String createdAt;
   final String name;
   final String avatar;
